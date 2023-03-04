9b26c984e7fe9209e632017325a0aef29483a735
Никита Крайнев <37338741+WebDevRu@users.noreply.github.com>
2023-03-04 18:25:40 +0300

    save

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -9,6 +9,26 @@ class Config {
         } else {
             this.url = url;
         }
+
+        const a = 2;
+        switch (a)
+        {
+            // Для пустых операторов case разрешено "проваливание" от одного оператора к другому.
+            case '1':
+            case "2": console.log("Этот день недели - Рабочий.");
+            case "3":
+            case "4":
+            case "5":
+                console.log("Этот день недели - Рабочий.");
+                break;
+            case "6":
+            case "7":
+                console.log("Этот день недели - Выходной.");
+                break;
+            default:
+                console.log("Вы ввели несуществующий день недели.");
+                break;
+        }
     }
 }
 
